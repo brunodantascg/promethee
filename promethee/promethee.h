0d02b8beedd397f2a0af8e1f0989599f021d32a1
Bruno Dantas <brunodedantas@gmail.com>
2019-07-19 14:57:04 -0300

    Projeto promethee dois

---
+++ promethee/promethee.h
@@ -0,0 +1,13 @@
+#ifndef PROMETHEE_INCLUDED
+  
+  #define PROMETHEE_INCLUDED
+  
+  #include "types.h"
+  #include "data.h"
+  #include <algorithm>
+  
+  struct Promethee {
+    PrometheeResult  process(Data data);
+  };
+
+#endif
